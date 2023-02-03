3dfc324a0c7e3e637c1883330ecf1769b3e97b7e
OlivMagno <olivmagno1@gmail.com>
2023-02-03 17:02:05 -0300

    installed fontawesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+/*fontawesome*/
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+library.add(faChevronRight);
+library.add(faChevronLeft);
+
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
