f340d06064e1e64deb2bc41b1a185a6d15b18220
OlivMagno <olivmagno1@gmail.com>
2023-02-03 19:49:41 -0300

    added a few layout adjustments

--- src/views/KnowledgesView.vue
+++ src/views/KnowledgesView.vue
@@ -13,9 +13,21 @@
     </div>
     <div class="skill-list">
       <div class="skill-item" v-for="(skill, index) in skillData" :key="index">
+        <font-awesome-icon :icon="skill.icon" class="fa-3x small-text" />
         <h2>{{ skill.tech }}</h2>
         <p class="large-text">{{ skill.description }}</p>
-        <p class="small-text">{{ skill.level }}</p>
+        <div class="level-container">
+          <p>{{ skill.levelName }}</p>
+          <div class="level-graph">
+            <span
+              v-for="i in (0, 3)"
+              :key="i"
+              :class="{
+                empty: checkBar(i, index),
+              }"
+            />
+          </div>
+        </div>
         <p class="large-text">{{ skill.familiarity }}</p>
       </div>
     </div>
@@ -28,29 +40,67 @@ export default {
   setup() {
     const skillData = [
       {
+        icon: "fa-brands fa-vuejs",
         tech: "Vue.js",
         description:
           "Vue é um framework de front-end que facilita a produção de aplicações, unindo o melhor do html, css e javascript em uma única ferramenta poderosa",
-        level: "2",
+        levelName: "intermediário",
+        levelNum: 2,
         familiarity:
           "Consigo criar aplicações, integrar com back-end com facilidade",
       },
       {
+        icon: "fa-brands fa-vuejs",
+        tech: "Vue.js",
+        description:
+          "Vue é um framework de front-end que facilita a produção de aplicações, unindo o melhor do html, css e javascript em uma única ferramenta poderosa",
+        levelName: "intermediário",
+        levelNum: 2,
+        familiarity:
+          "Consigo criar aplicações, integrar com back-end com facilidade",
+      },
+      {
+        icon: "fa-brands fa-vuejs",
+        tech: "Vue.js",
+        description:
+          "Vue é um framework de front-end que facilita a produção de aplicações, unindo o melhor do html, css e javascript em uma única ferramenta poderosa",
+        levelName: "intermediário",
+        levelNum: 2,
+        familiarity:
+          "Consigo criar aplicações, integrar com back-end com facilidade",
+      },
+      {
+        icon: "fa-brands fa-vuejs",
+        tech: "Vue.js",
+        description:
+          "Vue é um framework de front-end que facilita a produção de aplicações, unindo o melhor do html, css e javascript em uma única ferramenta poderosa",
+        levelName: "intermediário",
+        levelNum: 2,
+        familiarity:
+          "Consigo criar aplicações, integrar com back-end com facilidade",
+      },
+      {
+        icon: "fa-brands fa-square-js",
         tech: "Javascript",
         description:
           "Javascript é uma poderosa ferramenta de programação que permite uma flexibilidade de desenvolvimento",
-        level: "2",
+        levelName: "intermediário",
+        levelNum: 2,
         familiarity: "Intermediário",
       },
     ];
-    return { skillData };
+
+    const checkBar = (bar, index) => {
+      if (bar <= skillData[index].levelNum) return false;
+      else return true;
+    };
+    return { skillData, checkBar };
   },
 };
 </script>
 
 <style scoped>
 .skill-list {
-  background-color: var(--primary);
   width: 89.4rem;
   height: 35.8rem;
   margin: 13rem 0 0 5rem;
@@ -67,18 +117,35 @@ export default {
   flex-flow: row nowrap;
   align-items: flex-end;
   justify-content: flex-end;
+  transition: 0.2s;
+}
+
+.skill-item:hover {
+  transform: scale(1.01);
+  background-color: var(--primary);
 }
 
 .skill-item h2 {
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
+  justify-content: flex-start;
+  cursor: default;
+
+  height: 4rem;
+  width: 10rem;
+  margin: 0 1rem 0 0;
+}
+
+.skill-item .large-text {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
   justify-content: center;
 
   height: 4rem;
-  width: 16.5rem;
-  margin: 0 1rem 0 0;
-  background-color: white;
+  width: 30.45rem;
+  margin: 0 1rem 0 1rem;
 }
 
 .skill-item .small-text {
@@ -90,17 +157,44 @@ export default {
   height: 4rem;
   width: 5.5rem;
   margin: 0 1rem 0 0;
-  background-color: gray;
 }
-.skill-item .large-text {
+
+.level-container {
   display: flex;
-  flex-flow: row nowrap;
+  flex-flow: column nowrap;
   align-items: center;
   justify-content: center;
-
   height: 4rem;
-  width: 31.5rem;
-  margin: 0 1rem 0 0;
-  background-color: gray;
+  color: var(--accent);
+}
+
+.level-container p {
+  cursor: default;
+  transition: 0.6s;
+  opacity: 0;
+}
+
+.level-container:hover p {
+  opacity: 0.6;
+}
+
+.level-graph {
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: center;
+  width: 10rem;
+}
+
+span {
+  margin: 0.1rem;
+
+  width: 2rem;
+  height: 0.2rem;
+  background-color: var(--clear);
+}
+
+.empty {
+  opacity: 0.4;
 }
 </style>
